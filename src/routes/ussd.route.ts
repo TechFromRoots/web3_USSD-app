ad74b9d0d9bd777cc865243dad987662fcb9f51c
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-10-13 06:41:19 +0100

    Feat: Add base endpoint for cronjob

--- src/routes/ussd.route.ts
+++ src/routes/ussd.route.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import UssdController from '../controllers/ussd.controller';
 const router = express.Router();
 const { ussd } = new UssdController();
 
 // ussd gateway
 router.post("/", ussd);
+
+// base endpoint
+router.get("/", (_req: Request, res: Response) => {
+    res.send("App up and running...");
+});
 
 export default router;
